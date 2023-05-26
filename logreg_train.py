2a4a0e1cef0684e737ae23bd263c037c0b4c579f
Thomas BERTHET <thoberth@students.42.fr>
2023-05-26 17:15:18 +0200

    WIP at school

--- logreg_train.py
+++ logreg_train.py
@@ -1,3 +1,4 @@
+from tqdm import tqdm
 from utils import *
 
 def normalize(x) -> np.ndarray:
@@ -7,16 +8,49 @@ def normalize(x) -> np.ndarray:
 	return x_norm
 
 def predict(X, theta):
-	pass
+	X_extented = np.hstack((np.ones((X.shape[0], 1)), X))
+	y_hat = 1 / (1 + np.e ** - X_extented.dot(theta))
+	return y_hat
 
-def gradient(X, y):
-	pass
+def compute_gradient(X, y, theta):
+	X_extented = np.hstack((np.ones((X.shape[0], 1)), X))
+	log_gradient = (1/len(y)) * X_extented.T.dot(predict(X, theta) - y)
+	return log_gradient
 
-def fit(X, y, alpha=1e-3, iter=10000):
-	pass
+def fit(X, y, alpha=1e-3, iter=15000):
+	theta = np.random.rand(X.shape[1] + 1, 1).reshape(-1, 1)
+	print('Entrainement du modele:')
+	for _ in tqdm(range(iter)):
+		log_gradient = compute_gradient(X, y, theta)
+		theta = theta - (alpha * log_gradient)
+	return theta
 
 def loss(y, y_hat, eps=1e-15):
-	pass
+	log_loss = (y * np.log(y_hat + eps)) +\
+			((1 - y) * np.log(1 - y_hat + eps))
+	return (-1/len(y)) * np.sum(log_loss)
+
+def sort_y(y_to_sort, target):
+	y = np.zeros(y_to_sort.shape)
+	for i, is_target in enumerate(y_to_sort):
+		if is_target == target:
+			y[i] = 1
+	return y
+
+def sort_yhat(preds):
+	y_hat = np.zeros(preds[0].shape)
+	print(y_hat.shape)
+	for i, pred_0, pred_1, pred_2, pred_3 in zip(range(len(y_hat)), preds[0], preds[1], preds[2], preds[3]):
+		best = max(pred_0, pred_1, pred_2, pred_3)
+		if best == pred_0:
+			y_hat[i] = 0
+		if best == pred_1:
+			y_hat[i] = 1
+		if best == pred_2:
+			y_hat[i] = 2
+		if best == pred_3:
+			y_hat[i] = 3
+	return y_hat
 
 if __name__=="__main__":
 	if len(sys.argv) != 2:
@@ -27,5 +61,29 @@ if __name__=="__main__":
 	except:
 		print('Error while opening the file, please make sure your file is a .csv and the path is correct')
 		exit(1)
-	df.drop('Index', axis=1, inplace=True)
 	# Choisir les données significatives grace au pair plot puis les stockés et les normalizer dans X
+	# ne pas prendre 'Care of Magical Creature' et 'Arithmancy' et peut etre 'Potions'
+	df.drop(['Index', 'Care of Magical Creatures', 'Arithmancy'], axis=1, inplace=True)
+	df.dropna(axis=0, inplace=True)
+	y = df['Hogwarts House'].to_numpy().reshape(-1, 1)
+	X = None
+	for col in df.columns:
+		if is_numeric_column(df, col) == True:
+			if type(X) != np.ndarray:
+				X = df[col].to_numpy().reshape(-1, 1)
+			else:
+				X = np.c_[X, df[col].to_numpy().reshape(-1, 1)]
+	for i in range(X.shape[1]):
+		X[:, i] = normalize(X[:, i])
+	legend = { 0:'Gryffindor', 1:'Hufflepuff', 2:'Ravenclaw', 3:'Slytherin' }
+	theta = []
+	preds = []
+	for i in range(4):
+		theta.append(fit(X[:1000], sort_y(y, legend[i])[:1000]))
+		preds.append(predict(X[1000:], theta[i]))
+	y_hat = sort_yhat(preds)
+	pred_stat = 0
+	for yy, yy_hat in zip(sort_y(y, legend[i])[1000:], y_hat):
+		if yy == yy_hat:
+			pred_stat += 1
+	print(f'score : {pred_stat}/307')
--- pair_plot.py
+++ pair_plot.py
@@ -4,5 +4,5 @@ import seaborn as sns
 if __name__=="__main__":
 	df = pd.read_csv('datasets/dataset_train.csv')
 	df.drop('Index', axis=1, inplace=True)
-	sns.pairplot(df, height=1, aspect=2.5, hue='Hogwarts House', dropna=True)
+	sns.pairplot(df, height=1, aspect=1.5, hue='Hogwarts House', dropna=True)
 	plt.show()
